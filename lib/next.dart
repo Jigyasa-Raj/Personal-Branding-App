b2c7e072a66b229b492c8ee363e1ae1ad9928f1f
Jigyasa-Raj <92519474+Jigyasa-Raj@users.noreply.github.com>
2022-12-29 19:54:24 +0530

    Update next.dart

--- lib/next.dart
+++ lib/next.dart
@@ -10,30 +10,37 @@ class Next extends StatelessWidget {
     return Scaffold(
             appBar: AppBar(
               title: const Center(child: Text('All About Me')),
-              backgroundColor: Colors.teal[500],
+              backgroundColor: Colors.black,
             ),
-             backgroundColor: Colors.pink[100],
+             backgroundColor: Colors.cyanAccent[100],
 
            
-            body: ListView(children: [
-              Container(
-                padding: const EdgeInsets.all(10.0),
+            body: ListView(children: [Column(children: const <Widget>[
+                CircleAvatar(
+                  radius: 55,
+                  backgroundImage: AssetImage('images/pfp.jpg'),
+                ),
+                Text(
+                  'Xyz Abc',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+              ]),
+               Container(
+                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-                color: Colors.white,
+                color: Colors.black,
                 child: Row(
                   children: [
-                    const Icon(
-                      Icons.contact_mail,
-                      color: Colors.teal,
-                      size: 25.0,
-                    ),
                     Text(
-                      '\t\t\tSenior Software Developer',
+                      'ph: 9123456789',
                       style: TextStyle(
                         fontFamily: 'SourceSansPro',
                         fontSize: 20.0,
                         letterSpacing: 2.0,
-                        color: Colors.teal[900],
+                        color: Colors.cyanAccent[100],
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -43,39 +50,69 @@ class Next extends StatelessWidget {
               Container(
                 padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-                color: Colors.white,
+                color: Colors.black,
                 child: Row(
                   children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                      size: 25.0,
-                    ),
+                   
                     Text(
-                      '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tContacts',
+                      'email: adc@abc.com',
                       style: TextStyle(
                           fontFamily: 'SourceSansPro',
                           fontSize: 20.0,
                           letterSpacing: 2.0,
-                          color: Colors.teal[900],
+                          color: Colors.cyanAccent[100],
                           fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
+              ), Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.black,
+                child: Row(
+                  children: [
+                   
+                    Text(
+                      'website: abc.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.cyanAccent[100],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.black,
+                child: Row(
+                  children: [
+                    Text(
+                      'city: Chennai',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.cyanAccent[100],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
               ),
-              Container(
+              
+               Container(
                 padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
                 color: Colors.white,
                 child: Row(
                   children: [
-                    Icon(
-                      Icons.mail,
-                      color: Colors.teal,
-                      size: 25.0,
-                    ),
+                   
                     Text(
-                      '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tWebsite',
+                      'skills: flutter, figma',
                       style: TextStyle(
                           fontFamily: 'SourceSansPro',
                           fontSize: 20.0,
@@ -86,6 +123,109 @@ class Next extends StatelessWidget {
 
                   ],
                 ),
-             )]));
+              ),
+               Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                
+                    Text(
+                      'languages: Python, C++',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),
+               Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                  
+                    Text(
+                      'hobbies: Reading',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),
+               Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                   
+                    Text(
+                      'education: BTech CSE',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),
+               Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                  
+                    Text(
+                      'projects: Branding App',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),
+               
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                   
+                    Text(
+                      'experience: 4 years',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                  ],
+                ),
+              ),
+              
+             ]));
   }
 }
